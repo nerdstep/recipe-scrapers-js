0b9457d7ff1c6beed25f986b732846302591608a
Justin Williams <justinwilliams42@gmail.com>
2025-07-06 11:02:35 -0700

    chore: cleanup old code

--- src/scrapers/bbcgoodfood.ts
+++ src/scrapers/bbcgoodfood.ts
@@ -11,16 +11,6 @@ export class BBCGoodFood extends AbstractScraper {
     ingredients: this.ingredients.bind(this),
   }
 
-  /**
-   * The NYTimes website appears to auto generate it's CSS class names,
-   * which results in them ending with a string a random characters.
-   * Matching the exact class name is likely to break fairly quickly
-   * so instead we are going to match on a partial class name.
-   * For example, h3[class*='ingredientgroup_name'] matches an h3 element
-   * with a class that contains the value 'ingredient_groupname' at least once
-   * anywhere in the element class attribute.
-   * @link https://developer.mozilla.org/en-US/docs/Web/CSS/Attribute_selectors
-   */
   protected ingredients(
     prevValue: RecipeFields['ingredients'] | undefined,
   ): RecipeFields['ingredients'] {
--- src/scrapers/nytimes.ts
+++ src/scrapers/nytimes.ts
@@ -11,19 +11,10 @@ export class NYTimes extends AbstractScraper {
     ingredients: this.ingredients.bind(this),
   }
 
-  /**
-   * The NYTimes website appears to auto generate it's CSS class names,
-   * which results in them ending with a string a random characters.
-   * Matching the exact class name is likely to break fairly quickly
-   * so instead we are going to match on a partial class name.
-   * For example, h3[class*='ingredientgroup_name'] matches an h3 element
-   * with a class that contains the value 'ingredient_groupname' at least once
-   * anywhere in the element class attribute.
-   * @link https://developer.mozilla.org/en-US/docs/Web/CSS/Attribute_selectors
-   */
   protected ingredients(
     prevValue: RecipeFields['ingredients'] | undefined,
   ): RecipeFields['ingredients'] {
+    // Use wildcard selectors to handle dynamic class name suffixes
     const headingSelector = 'h3[class*="ingredientgroup_name"]'
     const ingredientSelector = 'li[class*="ingredient"]'
 
